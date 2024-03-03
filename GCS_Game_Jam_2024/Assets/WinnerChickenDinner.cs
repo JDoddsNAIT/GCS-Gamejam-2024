65c90e4b64ee1e3055f4cb82ff94de3a135e98bc
RordyC <rordycrawley@outlook.com>
2024-03-03 07:54:42 -0700

    FINAL COMMIT
     NO MORE

--- GCS_Game_Jam_2024/Assets/WinnerChickenDinner.cs
+++ GCS_Game_Jam_2024/Assets/WinnerChickenDinner.cs
@@ -2,8 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using UnityEditor.Build.Content;
-using UnityEngine.SceneManagement;
 
 public class WinnerChickenDinner : MonoBehaviour
 {
