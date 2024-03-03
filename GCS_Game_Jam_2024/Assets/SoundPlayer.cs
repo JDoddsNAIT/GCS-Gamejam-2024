edcec1a04668c67c4871991a590a11de54566586
JDoddsNAIT <144161210+JDoddsNAIT@users.noreply.github.com>
2024-03-02 18:14:37 -0700

    Update SoundPlayer.cs

--- GCS_Game_Jam_2024/Assets/SoundPlayer.cs
+++ GCS_Game_Jam_2024/Assets/SoundPlayer.cs
@@ -28,9 +28,4 @@ public class SoundPlayer : MonoBehaviour
         audioSource.clip = clipList[idx];
         audioSource.Play();
     }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        PlaySoundIndex(1);
-    }
 }
